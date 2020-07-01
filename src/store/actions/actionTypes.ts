04ea5f3b4bf2c148b18de9c92aa1908a265a34c4
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-01 17:54:31 +0200

    Add login type

--- src/store/actions/actionTypes.ts
+++ src/store/actions/actionTypes.ts
@@ -1,1 +1,11 @@
-export type AppActions = { type: string };
+import { User } from '../state';
+
+export const LOGIN = 'LOGIN';
+
+export interface LoginAction {
+    type: typeof LOGIN
+    user?: User
+    token?: string
+}
+
+export type Actions = LoginAction;
