346f6f4cd72393a5c2247bd0c591241802efc066
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-01 18:01:30 +0200

    Add custom logout action

--- src/store/actions/actionTypes.ts
+++ src/store/actions/actionTypes.ts
@@ -2,10 +2,16 @@ import { User } from '../state';
 
 export const LOGIN = 'LOGIN';
 
+export const LOGOUT = 'LOGOUT';
+
 export interface LoginAction {
     type: typeof LOGIN
     user?: User
     token?: string
 }
 
-export type Actions = LoginAction;
+export interface LogoutAction {
+    type: typeof LOGOUT
+}
+
+export type Actions = LoginAction | LogoutAction;
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -5,3 +5,7 @@ import { User } from '../state';
 export function login(user: User, token: string): types.LoginAction {
     return { type: types.LOGIN, user, token };
 }
+
+export function customLogout(): types.LogoutAction {
+    return { type: types.LOGOUT };
+}
