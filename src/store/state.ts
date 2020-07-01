ea09257720396b75ace621e8791e1eb1c0eb3cfe
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-01 17:54:01 +0200

    Draft user state

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,9 +1,24 @@
-export interface SystemState {
+export interface User {
+    email: string
+    email_verified: boolean
+    family_name: string
+    given_name: string
+    locale: string
+    name: string
+    nickname: string
+    picture: string
+    sub: string
+    updated_at: string
+}
 
+export interface SystemState {
+    user?: User
+    token?: string
 }
 
 const initialState: SystemState = {
-
+    user: null,
+    token: null,
 };
 
 export default initialState;
