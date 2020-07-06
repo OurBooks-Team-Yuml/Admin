da144b59a02e80131c1cc6d37c8672342ae17efc
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-06 18:28:45 +0200

    Draft book list component

---
+++ src/components/Books.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { useQuery } from '@apollo/react-hooks';
+
+import { showLoader } from '../store/actions';
+import { GET_ALL_BOOKS } from '../graphql/queries';
+import { Author, Book } from '../graphql/schemas';
+
+const Books : FC = () => {
+    const dispatch = useDispatch();
+    const { loading, error, data } = useQuery(GET_ALL_BOOKS);
+
+    if (loading) {
+        dispatch(showLoader);
+    }
+
+    if (error) {
+        // TODO SHOW ERROR TO USER.
+        console.log(error);
+    }
+
+    return (
+        <div>
+            {data !== undefined && (
+                <table className="table is-stripped is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>Test</th>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Authors</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.books.map((book: Book) => (
+                            <tr>
+                                <td><img src={book.imagePath} /></td>
+                                <td>{book.id}</td>
+                                <td>{book.name}</td>
+                                <td>
+                                    {book.authorsId.map((author: Author) => (
+                                        <span>
+                                            {author.fullName}
+                                        </span>
+                                    ))}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
+};
+
+export default Books;
