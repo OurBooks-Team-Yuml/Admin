307ec7248c0a004d0fb02b91c3dfaaa1fd92b8cb
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-06 19:05:47 +0200

    Use secured endpoint, add Authorization header to request

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,21 @@
 import ApolloClient from 'apollo-boost';
 
+import configureStore from '../store';
+
+const { store } = configureStore();
+
 const client = new ApolloClient({
-    uri: 'http://localhost:8001/books/authorized/',
+    uri: 'http://localhost:8000/books/secured/',
+    request: (operation) => {
+        const state = store.getState();
+        const { token } = state.app;
+
+        operation.setContext({
+            headers: {
+                authorization: token ? `Bearer ${token}` : '',
+            },
+        });
+    },
 });
 
 export default client;
