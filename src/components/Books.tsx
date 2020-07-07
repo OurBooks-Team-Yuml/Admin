d468febc0f5c6a282b60f89aedb3da76fccb2f69
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-07 12:45:06 +0200

    Add simple pagination

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { useQuery } from '@apollo/react-hooks';
 
@@ -7,9 +8,25 @@ import { showLoader } from '../store/actions';
 import { GET_ALL_BOOKS } from '../graphql/queries';
 import { Author, Book } from '../graphql/schemas';
 
-const Books : FC = () => {
+import { Match } from '../types';
+
+interface BooksParams {
+    page: string
+}
+
+interface MatchWithParams extends Match {
+    params: BooksParams
+}
+
+interface Props {
+    match: MatchWithParams
+}
+
+const Books : FC<Props> = (props) => {
+    const page = parseInt(props?.match?.params.page, 10);
     const dispatch = useDispatch();
-    const { loading, error, data } = useQuery(GET_ALL_BOOKS);
+    const { loading, error, data } = useQuery(GET_ALL_BOOKS(page));
+    const { data: nextPage } = useQuery(GET_ALL_BOOKS(page + 1));
 
     if (loading) {
         dispatch(showLoader);
@@ -22,33 +39,68 @@ const Books : FC = () => {
 
     return (
         <div>
-            {data !== undefined && (
-                <table className="table is-stripped is-fullwidth">
-                    <thead>
-                        <tr>
-                            <th>Test</th>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Authors</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.books.map((book: Book) => (
-                            <tr key={book.id}>
-                                <td><img src={book.imagePath} /></td>
-                                <td>{book.id}</td>
-                                <td>{book.name}</td>
-                                <td>
-                                    {book.authorsId.map((author: Author) => (
-                                        <span key={author.id}>
-                                            {author.fullName}
-                                        </span>
-                                    ))}
-                                </td>
+            {(data !== undefined && nextPage !== undefined) && (
+                <div>
+                    <table className="table is-stripped is-fullwidth">
+                        <thead>
+                            <tr>
+                                <th>Test</th>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Authors</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {data.books.map((book: Book) => (
+                                <tr key={book.id}>
+                                    <td><img src={book.imagePath} /></td>
+                                    <td>{book.id}</td>
+                                    <td>{book.name}</td>
+                                    <td>
+                                        {book.authorsId.map((author: Author) => (
+                                            <span key={author.id}>
+                                                {author.fullName}
+                                            </span>
+                                        ))}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                    <nav className="pagination" role="navigation" aria-label="pagination">
+                        {page === 1 ? (
+                            <Link
+                                className="pagination-previous disabled"
+                                to="/book-list/1"
+                            >
+                                Previous
+                            </Link>
+                        ) : (
+                            <Link
+                                className="pagination-previous"
+                                to={`/book-list/${page - 1}`}
+                            >
+                                Previous
+                            </Link>
+                        )}
+
+                        {!nextPage.books.length ? (
+                            <Link
+                                className="pagination-next disabled"
+                                to={`/book-list/${page + 1}`}
+                            >
+                                Next page
+                            </Link>
+                        ) : (
+                            <Link
+                                className="pagination-next"
+                                to={`/book-list/${page + 1}`}
+                            >
+                                Next page
+                            </Link>
+                        )}
+                    </nav>
+                </div>
             )}
         </div>
     );
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Match {
+    isExact: boolean
+    path: string
+    url: string
+}
